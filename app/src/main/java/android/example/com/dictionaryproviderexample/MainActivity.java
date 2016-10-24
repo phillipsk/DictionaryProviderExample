d11a37816e270d971571d020eb560f5a70a9a5b4
phillipsK <kjphillips@suffolk.edu>
2016-10-24 19:56:25 -0400

    cursor.getColumnIndex() and .getInt() and .getString() to append the Dictionary ContentURI onto the TextView

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -42,22 +42,28 @@ public class MainActivity extends ActionBarActivity {
 
         // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
-        
+
         // Surround the cursor in a try statement so that the finally block will eventually execute
         try {
-            dictTextView.setText("The UserDictionary contains ");
+            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words\n");
             // -- YOUR CODE BELOW HERE -- //
+            dictTextView.append("Columns: " + Words._ID + " - " + Words.FREQUENCY +
+                    " - " + Words.WORD);
 
             // Get the index of the column containing the actual words, using
             // UserDictionary.Words.WORD, which is the header of the word column.
             int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+            int idColumn = cursor.getColumnIndex(Words._ID); /* shorthand */
+            int freqColumn = cursor.getColumnIndex(Words.FREQUENCY);
 
             // Iterates through all returned rows in the cursor.
             while (cursor.moveToNext()) {
                 // Use that index to extract the String value of the word
                 // at the current row the cursor is on.
                 String word = cursor.getString(wordColumn);
-                dictTextView.append(("\n" + word));
+                int id = cursor.getInt(idColumn);
+                int freq = cursor.getInt(freqColumn);
+                dictTextView.append(("\n" + id + " - " + freq + " - " + word));
             }
         } finally {
             // Always close your cursor to avoid memory leaks
